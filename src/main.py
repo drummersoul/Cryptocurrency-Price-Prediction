3ae91df581c0da9df461fb13c4736f7a23a5c5b5
sushma <sushma23042001@gmail.com>
2024-04-11 17:40:27 -0400

    created seperate class for dashbord and added knn tabs in dashboard

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 from data_understanding import DataUnderstanding
+from dashboard import Dashboard
 
 def run(file_name : str = 'crypto_data_info.csv'):
     du = DataUnderstanding()
+    dashboard = Dashboard()
     print("******************** Start ********************")
     raw_data = du.load_and_filter_data(file_name)
     cleaned_data = du.clean_data(raw_data)
     cleaned_data = du.handle_missing_data(cleaned_data)
     data_with_new_features = du.create_features(cleaned_data)
     du.data_understanding(data_with_new_features)
-    du.train_models(data_with_new_features)
+    res = du.train_models(data_with_new_features)
+    dashboard.dashboard(res[0], res[1], res[2], res[3], res[4])
     # evaluate
     print("******************** End ********************")
 
